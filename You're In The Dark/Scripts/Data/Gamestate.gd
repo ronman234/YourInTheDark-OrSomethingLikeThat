dfc8fb3d25b844e9bcbe6e8ba1ec3f95147d4437
TrevtheForthDev <hilliblockmrblock@gmail.com>
2022-08-22 21:39:52 -0600

    Fixed problems with compile-time resource loading

--- You're In The Dark/Scripts/Data/Gamestate.gd
+++ You're In The Dark/Scripts/Data/Gamestate.gd
@@ -3,10 +3,8 @@ extends Node
 class_name game_state
 
 var current_scene : Scene = null
-var test_scene : Scene = preload("res://SceneList/Titlescreen.tres")
 var random : RandomNumberGenerator = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	Master.game_instance = self
 	random.randomize()
-
