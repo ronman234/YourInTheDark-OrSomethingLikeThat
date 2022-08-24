c0dc8d4cfcf435b39f336a4e6ea04a6dff0e2cfb
TrevtheForthDev <hilliblockmrblock@gmail.com>
2022-08-24 02:27:54 -0600

    Added new hall gen

--- You're In The Dark/Scripts/Interaction/Door.gd
+++ You're In The Dark/Scripts/Interaction/Door.gd
@@ -3,16 +3,24 @@ extends Spatial
 #class_name Door
 
 var corresponding_object
-var spawn
-
+var spawn = Spatial.new()
+var hall : Hall
 var is_active : bool = false
 var can_open : bool = true
-func activate():
-	spawn = corresponding_object
+#var current_door = false
+func _ready():
 	add_child(spawn)
+func activate():
 	if can_open:
+		spawn.add_child(corresponding_object)
 		$AnimationPlayer.play("open")
 		is_active = true
+		print(str("MANSION DOOR PRE: ", hall.mansion.current_door))
+		hall.mansion.current_door = self
+		print(str("MANSION DOOR POST: ", hall.mansion.current_door))
 func deactivate():
 	$AnimationPlayer.play("close")
-#	can_open = false
+	corresponding_object = null
+#	current_door = false
+func get_content():
+	return spawn.get_child(0)
