9bd62ca2db8e9d1a6bab0438df41cb2acccb7274
Chrisopher Bell <christopherisaacbell@gmail.com>
2022-08-24 22:28:14 -0500

    Added address numbers to address generation, and changed the name

--- You're In The Dark/Scripts/GenerateAddress.gd
+++ You're In The Dark/Scripts/GenerateAddress.gd
@@ -3,6 +3,7 @@ extends Spatial
 onready var prefix_label : Label = $PrefixLabel
 onready var suffix_label : Label = $SuffixLabel
 
+var current_address_number : String
 var current_address_name : String
 
 export var prefixs : Array = [
@@ -64,15 +65,19 @@ export var suffixs : Array = [
 
 func generate_address_name():
 	var rand : RandomNumberGenerator = Master.get_game().random
+	var address_number : int = rand.randi_range(10, 9999)
 	var prefix_text : String = prefixs[rand.randi_range(0, prefixs.size() - 1)]
 	var suffix_text : String = suffixs[rand.randi_range(0, suffixs.size() - 1)]
 	prefix_label.text = prefix_text
 	suffix_label.text = suffix_text
-	current_address_name = str(prefix_text, " ", suffix_text)
+	current_address_number = str(address_number)
+	current_address_name = str(address_number, " ", prefix_text, " ", suffix_text)
+	print(current_address_name)
+	return current_address_name
 	
 	
 func _ready():
-	generate_address_name()
+	print (generate_address_name())
 
 
 func _process(_delta):
